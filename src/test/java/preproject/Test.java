7fe36ca70a9f1aab41747936dfcf045b2936ac50
shaikfaruk <shaikfaruk04@gmail.com>
2020-10-22 16:50:41 +0530

    first code

---
+++ src/test/java/preproject/Test.java
@@ -0,0 +1,16 @@
+package preproject;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Test {
+
+	@org.testng.annotations.Test
+	 public void launchingbrowser(){
+		 System.setProperty("webdriver.chrome.driver","D:\\chromedriver_win32\\chromedriver.exe");
+		  WebDriver driver= new ChromeDriver();
+		   driver.get("https://www.google.com");
+	}
+	
+	
+}
