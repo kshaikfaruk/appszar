2da385bbf889183c7e55090888d84c58505fc4df
shaikfaruk <shaikfaruk04@gmail.com>
2020-10-22 20:37:00 +0530

    commit

--- src/test/java/preproject/Test.java
+++ src/test/java/preproject/Test.java
@@ -1,7 +1,10 @@
 package preproject;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Test {
@@ -13,9 +16,9 @@ public class Test {
 		   driver.get("https://www.google.com");
 		   driver.manage().window().maximize();
 		    driver.findElement(By.name("q")).sendKeys("fb");
-		    
-		     
-		   
+		    driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		    WebElement element= driver.findElement(By.xpath("//*[@id='tsf']/div[2]/div[1]/div[3]/center/input[1]"));
+		    element.click();
 		   
 		   
 		   
